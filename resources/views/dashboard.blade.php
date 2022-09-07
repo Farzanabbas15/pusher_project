f84a165851114c827adf36e4741264fbd4565ec3
syed farzan abbas <farzanabbas3@gmail.com>
2022-09-07 10:28:54 -0700

    pusher_second

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
       <div class="row justify-contect-center">
         <div class="col-12 col-sm-6 col-md-6">
-          <div class="form-container">
+          <form class="form-container">
             <h2 class ="text-center bg-dark p-2 text-white">Add Text Listing</h2>
             <div class="form-group">
               <label for="title">Title</label>
@@ -18,16 +18,55 @@
               <label for="description">Description</label>
               <textarea class="form-control" id="description" 
               placeholder="Enter Description"></textarea>
-
-              <button type="submit" class="btn btn-dark btn-block">Submit</button>
             </div>
-          </div>
+
+            <button type="submit" class="btn btn-dark btn-block save_btn">Submit</button>
+            
+          </form>
         </div>
       </div>
 
     </div>
   </section>
 @endsection
+
+@push('javascript')
+<script>
+  $(document).ready(function(){
+    $('.save_btn').on('click', function(e){
+      let title =$('#title').val();
+      let description =$('#description').val();
+
+      const form = $(this).parents('form');
+
+      $(form).validate({
+        rules: {
+          title: {
+            required: ture
+          }
+        },
+        message:{
+          title:"Title is required"
+        },
+        submitHandler:function(){
+          var forData = new FormData(form[0]);
+
+          $.ajax({
+            type: 'POST',
+            url: 'save_task',
+            data: formData,
+            processData: false,
+            contentType: flase,
+            success:function(data){
+              
+            }
+          })
+        }
+      });
+    });
+  });
+</script>
+@endpush
 
 
 
